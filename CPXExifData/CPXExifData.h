fb708e5fb68ef8afe395b71359a1b813167af085
Fuji, Goro (gfx) <goro-fuji@cookpad.com>
2014-09-12 16:51:51 +0900

    Initial Commit

---
+++ CPXExifData/CPXExifData.h
@@ -0,0 +1,19 @@
+//
+//  CPXExifData.h
+//  CPXExifData
+//
+//  Created by goro-fuji on 9/12/14.
+//  Copyright (c) 2014 Cookpad Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CPXExifData.
+FOUNDATION_EXPORT double CPXExifDataVersionNumber;
+
+//! Project version string for CPXExifData.
+FOUNDATION_EXPORT const unsigned char CPXExifDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CPXExifData/PublicHeader.h>
+
+
